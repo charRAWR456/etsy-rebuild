5dd42cb06c7a32a5a322072575b0b0d6b71c0ceb
Jamie Pittak <charrawr456@gmail.com>
2018-03-30 15:14:26 -0700

    update router with paths for functioning nav bar

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,32 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { RegisterComponent } from './register/register.component';
+import { SellOnEtsyComponent } from './sell-on-etsy/sell-on-etsy.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: HomeComponent
   },
+  {
+    path: 'cart',
+    component: CartComponent
+  },
+  {
+    path: 'detail',
+    component: ItemDetailComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'sell',
+    component: SellOnEtsyComponent
+  }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
