054c1c860b82272a3093d8f2cac9ba942114760a
Jamie Pittak <charrawr456@gmail.com>
2018-03-30 10:50:10 -0700

    add etsy nav bar and favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { routing } from './app.routing';
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
 
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { SellOnEtsyComponent } from './sell-on-etsy/sell-on-etsy.component';
 import { RegisterComponent } from './register/register.component';
 import { CartComponent } from './cart/cart.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
     HomeComponent,
     SellOnEtsyComponent,
     RegisterComponent,
-    CartComponent
+    CartComponent,
+    ItemDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
