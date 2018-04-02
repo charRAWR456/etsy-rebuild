57b75f223a5a655f2f85a201fd41f77f61d8f0c3
Jamie Pittak <charrawr456@gmail.com>
2018-04-02 15:53:48 -0700

    update style of navbar 1, add navbar 2, update cards with image and styling

--- src/app/mock-items.ts
+++ src/app/mock-items.ts
@@ -1,11 +1,11 @@
 import { Item } from './item.model';
 
 export const ITEMS: Item[] = [
-   new Item("Vintage french classics book bundle", "RosiesArmoire","$36.23", 1),
-   new Item("RMS TITANIC 1912 Padlock Beautiful working model. Use this on your boat.", "shoretrader", '$34.95', 2),
-   new Item("French Vintage cross lot 2 crucifix in rosetree wood wooden crucifix metalcross jesus stamped France", "PARISOUIPARIS",'$8.50', 3),
-   new Item("Stainless Silverware Set Mismatched Flatware Cottage Chic Service for 12, 8, 4 or more, Unique", "LaurasLastDitch",
+   new Item("Vintage french classics", "RosiesArmoire","$36.23", 1),
+   new Item("RMS TITANIC 1912 Padlock", "shoretrader", '$34.95', 2),
+   new Item("French Vintage cross", "PARISOUIPARIS",'$8.50', 3),
+   new Item("Stainless Silverware Set", "LaurasLastDitch",
        '$49.99', 4),
-   new Item("Vintage 1970s Lladro Figurine 'Shepherd with Bird' 4730 - retired", "SecondWindShop",'$90.00', 5),
-   new Item("Cookie, A Royal Doulton Figurine (HN2218) (Retired)", "JacqsClassics",'$175.00', 6)
+   new Item("Vintage 1970s Lladro Figurine", "SecondWindShop",'$90.00', 5),
+   new Item("Cookie, A Royal Doulton Figurine", "JacqsClassics",'$175.00', 6)
  ];
