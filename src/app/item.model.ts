1bf3820036022b295c9a697f5d797975aff838f1
jamie pittak <charrawr456@gmail.com>
2018-04-03 13:26:39 -0700

    add shop by category and pet card

--- src/app/item.model.ts
+++ src/app/item.model.ts
@@ -1,5 +1,6 @@
 export class Item {
   constructor (
+    public url: string,
     public title: string,
     public shopName: string,
     public price: string,
--- src/app/mock-items.ts
+++ src/app/mock-items.ts
@@ -1,11 +1,11 @@
 import { Item } from './item.model';
 
 export const ITEMS: Item[] = [
-   new Item("Vintage french classics", "RosiesArmoire","$36.23", 1),
-   new Item("RMS TITANIC 1912 Padlock", "shoretrader", '$34.95', 2),
-   new Item("French Vintage cross", "PARISOUIPARIS",'$8.50', 3),
-   new Item("Stainless Silverware Set", "LaurasLastDitch",
-       '$49.99', 4),
-   new Item("Vintage 1970s Lladro Figurine", "SecondWindShop",'$90.00', 5),
-   new Item("Cookie, A Royal Doulton Figurine", "JacqsClassics",'$175.00', 6)
+   new Item("./../assets/images/necklace.jpg","Sterling octopus seawater necklace", "VillaSorgenfrei","$38.90", 1),
+   new Item("./../assets/images/records.jpg","World Travel Decor", "VinylShopUS", '$99.99', 2),
+   new Item("./../assets/images/wedding.jpg","Disney wedding guest book", "WeddingGuestBook3D",'$52.93', 3),
+   new Item("./../assets/images/pens.jpg","Bean sprout gel pen", "warmpersonality",
+       '$2.00', 4),
+   new Item("./../assets/images/worldscratch.jpg","World Scratch Your Travels", "KUULYS",'$29.99', 5),
+   new Item("./../assets/images/journal.jpg","Personalized Travel Journal Trip Journal", "PleaseAndThanksCards",'$28.00', 6)
  ];
